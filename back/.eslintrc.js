efc143c464ea4aa0da4ae156760672e08e1a27db
Joao Pedro <joao.sjc12@hotmail.com>
2020-06-15 16:01:51 -0300

    add prettier on project

--- back/.eslintrc.js
+++ back/.eslintrc.js
@@ -1,17 +1,19 @@
 module.exports = {
-    "env": {
-        "es2020": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
-    "rules": {
-        "class-methods-use-this": "off",
-        "no-param-reassing": "off",
-        "camelcase": "off",
-        "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
-    }
+  env: {
+    es2020: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  plugins: ["prettier"],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-param-reassing": "off",
+    camelcase: "off",
+    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
+  },
 };
